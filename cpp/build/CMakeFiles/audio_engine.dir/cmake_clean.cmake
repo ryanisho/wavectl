97c91f961f4e2839fff2308645fb94dd95be2c5b
Ryan Ho <rh564@cornell.ed>
2025-09-09 17:13:58 -0400

    Finish audio engine

---
+++ cpp/build/CMakeFiles/audio_engine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/audio_engine.dir/audio_engine.cpp.o"
+  "CMakeFiles/audio_engine.dir/audio_engine.cpp.o.d"
+  "audio_engine.cpython-312-darwin.so"
+  "audio_engine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/audio_engine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cpp/build/CMakeFiles/audio_engine.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for audio_engine.
+# This may be replaced when dependencies are built.
